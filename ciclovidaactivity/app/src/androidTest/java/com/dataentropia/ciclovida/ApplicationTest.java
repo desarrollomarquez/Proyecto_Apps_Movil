d945a87a747a69fe84d8adef80c945c213f59213
Diego Marquez <diego.marquezbetancur@tcs.com>
2020-03-09 20:44:59 -0500

    Proyecto basico Ciclo de vida Activity

---
+++ ciclovidaactivity/app/src/androidTest/java/com/dataentropia/ciclovida/ApplicationTest.java
@@ -0,0 +1,4 @@
+package com.dataentropia.ciclovida;
+
+import android.app.Application;
+
---
+++ ciclovidaactivity/app/src/main/java/com/dataentropia/ciclovida/controller/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.dataentropia.ciclovida.controller;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dataentropia.ciclovida.R;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo);
+    }
+}
